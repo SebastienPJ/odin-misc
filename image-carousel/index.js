56b40b05941be9d16a3a93509f6286a94a7a254e
Sebastien Pierre-Jacques <sebastienpj@protonmail.com>
2021-12-03 20:47:01 -0600

    animation test

--- image-carousel/index.js
+++ image-carousel/index.js
@@ -2,5 +2,30 @@ const rightArrow = document.querySelector('.right-arrow');
 const currentPicture = document.querySelector('.current-picture')
 
 rightArrow.addEventListener('click', function() {
+  console.log(this);
+  console.log(currentPicture);
   currentPicture.classList.add('fade-out')
+  setTimeout(() => {
+    currentPicture.classList.add('hide')
+    
+  }, 4000);
+
+  setTimeout(() => {
+    currentPicture.nextElementSibling.classList.toggle('hide')
+    currentPicture.nextElementSibling.classList.add('fade-in')
+    
+  }, 5000);
+
+
+
+  currentPicture.nextElementSibling.classList.toggle('current-picture')
+  currentPicture.classList.toggle('current-picture')
+  
+
 })
+
+
+
+function playAnimation(currentImage, nextImage) {
+
+}
